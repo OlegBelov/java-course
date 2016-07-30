8ad0e027d5bb2be0cbd8e0468b58b4448e439cf5
OlegBelov <belov-oleg-1993@mail.ru>
2016-07-30 15:47:54 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,64 @@
+/**
+ * Created by Oleg on 10.06.2016.
+ *
+ * Calculate
+ */
+public class Calculator {
+
+    private int result;
+
+    /**
+ * Summarizes the numbers
+ * @param params numbers
+     */
+    public void add(int ... params) {
+        for (Integer param : params) {
+            this.result += param;
+        }
+    }
+
+    /**
+ * Subtracts the numbers
+ * @param params numbers
+     */
+    public void sub(int ... params) {
+        for (Integer param : params) {
+            this.result -= param;
+        }
+    }
+
+    /**
+ * Multiplies the numbers
+ * @param params numbers
+     */
+    public void multiply(int ... params) {
+        for (Integer param : params) {
+            this.result *= param;
+        }
+    }
+
+    /**
+ * Divides the numbers
+ * @param params numbers
+     */
+    public void divide(int ... params) {
+        for (Integer param : params) {
+            this.result /= param;
+        }
+    }
+
+    /**
+ * Return the result
+ * @return the result
+     */
+    public int getResult() {
+        return this.result;
+    }
+
+    /**
+ * Clean the result
+     */
+    public void clearResult() {
+        this.result = 0;
+    }
+}
